f374544fc9c0b6cf4f210223a629a9ada88bc4f0
Prashik Deshpande <43670200+PrashikD@users.noreply.github.com>
2019-09-24 16:38:14 +0530

    Add files via upload

---
+++ Leadership traits by Bill Gates.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.inc.com/marcel-schwantes/bill-gates-says-this-1-simple-habit-separates-successful-leaders-from-everyone-else.html'  # news article link
+head = {'User-Agent': 'Chrome/50.0.2661.102'}
+req = requests.get(url, headers=head)
+
+soup = BeautifulSoup(req.text, "html.parser")
+
+points = soup.find_all('div', attrs={'element': 'h2'})
+
+for x in points:
+    if not x.text[0].isdigit():
+        points.remove(x)
+
+filtered_points = [str(x.text) for x in points]
+
+final_points = [x[3:] for x in filtered_points]
+
+print(*final_points, sep="\n")
