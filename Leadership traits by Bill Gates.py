17a5253a31f5bc17d25c8eb80bcbd935d4bcdb2a
Prashik Deshpande <43670200+PrashikD@users.noreply.github.com>
2019-09-24 16:38:54 +0530

    Update Leadership traits by Bill Gates.py

--- Leadership traits by Bill Gates.py
+++ Leadership traits by Bill Gates.py
@@ -18,3 +18,15 @@ filtered_points = [str(x.text) for x in points]
 final_points = [x[3:] for x in filtered_points]
 
 print(*final_points, sep="\n")
+
+''' 
+Leadership is not about titles or positional authority. 
+Leadership is not about personality traits.
+Leadership is not management.
+Leadership isn't about you.
+Great leaders allow people to fail.
+Great leaders create opportunities for people to thrive.
+Great leaders embrace respectful disagreement.
+Great leaders share leadership.
+
+'''
